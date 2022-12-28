8053632fe64a61304ee481fb567bc468be16ac4e
KarempudiKrishnaMurthy <112862797+KarempudiKrishnaMurthy@users.noreply.github.com>
2022-12-28 22:57:47 +0530

    Add files via upload

---
+++ 35. Search Insert Position.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        int low = 0;
+        int high=nums.size()-1;
+        int mid;
+        while(low<=high)//using binary search we can find the index of a sorted array
+        {
+            mid = low+ (high-low)/2;
+            if(nums[mid]==target)
+            return mid;
+            else if(nums[mid]<target)
+              low= mid+1;
+            else
+               high=mid-1;
+            
+        }
+        return low;
+
+    }
+};
